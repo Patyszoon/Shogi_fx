000a7e2d50491db9743c364a8cffe2fa0f200870
bspat <154008056+bspat@users.noreply.github.com>
2025-03-31 20:08:43 +0200

    Zasady dla gońca i promowanego gońca

--- src/main/java/bierki/Goniec.java
+++ src/main/java/bierki/Goniec.java
@@ -8,12 +8,18 @@ public class Goniec extends PromowalnaBierka{
 
     @Override
     protected boolean czyLegalnyPrzedPromocja(int x, int y) {
-        return false;
+        if(x == this.logX && y == this.logY)return false;
+        int odlegloscX = x - this.logX;
+        int odlegloscY = y - this.logY;
+        return (odlegloscY == odlegloscX) || ((odlegloscY * (-1)) == odlegloscX);
     }
 
     @Override
     protected boolean czyLegalnyPoPromocji(int x, int y) {
-        return super.czyLegalnyPoPromocji(x, y);
+        if(x == this.logX && y == this.logY-1)return true;
+        if(x == this.logX && y == this.logY+1)return true;
+        if(x == this.logX+1 && y == this.logY)return true;
+        return x == this.logX - 1 && y == this.logY;
     }
 
     public Goniec(Board board, Kolor kolor, Stan stanb, int wiersz, int kolumna) {
