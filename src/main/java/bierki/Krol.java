8fdc1537407133329cba0eed21a0d3a86ee7a73c
bspat <154008056+bspat@users.noreply.github.com>
2025-03-31 19:55:46 +0200

    Zasady ruchu dla króla

--- src/main/java/bierki/Krol.java
+++ src/main/java/bierki/Krol.java
@@ -5,11 +5,15 @@ import plansza.Board;
 public class Krol extends Bierka{
 
     @Override
-    public boolean czyLegalny(int x, int y) {
+    public boolean czyLegalny(int x, int y)
+    {
+        if(x == this.logX && y == this.logY) return false;
+        for(int xl = x-1; xl <= x+1; xl++)
+            for(int yl = y-1; yl <= y+1; yl++)
+                if(xl == this.logX && yl == this.logY) return true;
         return false;
     }
 
-    //do sprite'a bierki
     public Krol(Board board, Kolor kolor, Stan stanb, int wiersz, int kolumna) {
         super(board, kolor, stanb, wiersz, kolumna, 1);
     }
