dc632b4979d280385e3ee16270d01f393e83e107
bspat <154008056+bspat@users.noreply.github.com>
2025-03-31 20:13:57 +0200

    Zasady dla zlotego generala

--- src/main/java/bierki/ZlotyGeneral.java
+++ src/main/java/bierki/ZlotyGeneral.java
@@ -6,6 +6,12 @@ public class ZlotyGeneral extends Bierka{
 
     @Override
     public boolean czyLegalny(int x, int y) {
+        if(x == this.logX && y == this.logY)return false;
+        if(x == this.logX-1 && y == this.logY-1)return false;
+        if(x == this.logX+1 && y == this.logY-1)return false;
+        for(int xl = x-1; xl <= (x + 1); xl++)
+            for(int yl = y-1; yl <= (y + 1); yl++)
+                if(xl == this.logX && yl == this.logY) return true;
         return false;
     }
 
