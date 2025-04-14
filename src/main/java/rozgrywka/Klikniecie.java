7988244b4adeeb54a9ff5a61227987adc4477c1d
bspat <154008056+bspat@users.noreply.github.com>
2025-04-14 10:31:48 +0200

    Dodano klasę rozgrywka Jej celem jest wytworzenie interfejsu pomiędzy logiką a grafiką

---
+++ src/main/java/rozgrywka/Klikniecie.java
@@ -0,0 +1,8 @@
+package rozgrywka;
+
+public class Klikniecie {
+    //ta klasa powinna pozwolić na przekazanie parametrów z kliknięcia
+
+    //nie upieram się przy tym rozwiązaniu, równie dobrze można w inny sposób to rozwiązać,
+    //np od razu przeliczać na współrzędne logiczne i je przekazywać do rozgrywki
+}
---
+++ src/main/java/rozgrywka/Rozgrywka.java
@@ -0,0 +1,13 @@
+package rozgrywka;
+
+import bierki.*;
+
+public class Rozgrywka {
+
+
+    //metoda zwraca true, jeżeli klikniecie sprawia zmianę sytuacji wyświetlanej na planszy
+    public boolean ruch(Klikniecie klikniecie)
+    {
+        return false;
+    }
+}
